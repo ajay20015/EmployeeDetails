7c6da01218ed2f68cf96912a5219bccc66fbba9b
AJAY I <itiajay2001@gmail.com>
2023-01-02 10:06:49 +0530

    Add tooltip

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,12 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { AppComponent } from './app.component';
-import { EmpHomeComponent } from './EmpDetails/emp-home/emp-home.component';
-import { ErrorComponent } from './EmpDetails/error/error.component';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { EmpHomeComponent } from "./EmpDetails/emp-home/emp-home.component";
+import { ErrorComponent } from "./EmpDetails/error/error.component";
 
 const routes: Routes = [
-  { path: '', redirectTo: 'emp-home', pathMatch: 'full' },
-  { path: 'emp-home', component: EmpHomeComponent },
-  { path: '**', component: ErrorComponent },
+  { path: "", redirectTo: "emp-home", pathMatch: "full" },
+  { path: "emp-home", component: EmpHomeComponent },
+  { path: "**", component: ErrorComponent },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,19 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { EmpHomeComponent } from './EmpDetails/emp-home/emp-home.component';
-import { ErrorComponent } from './EmpDetails/error/error.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { EmpMaterialModuleModule } from './EmpDetails/emp-material-module/emp-material-module.module';
-import { HttpClientModule } from '@angular/common/http';
-import { ViewDialogComponent } from './EmpDetails/emp-home/view-dialog/view-dialog.component';
-import { EditDialogComponent } from './EmpDetails/emp-home/edit-dialog/edit-dialog.component';
-import { AddDialogComponent } from './EmpDetails/emp-home/add-dialog/add-dialog.component';
-import { DeleteDialogComponent } from './EmpDetails/emp-home/delete-dialog/delete-dialog.component';
-import { ReactiveFormsModule } from '@angular/forms';
-import { EmpCurdServiseService } from './EmpDetails/emp-curd-servise.service';
-import { PopupDialogComponent } from './EmpDetails/emp-home/popup-dialog/popup-dialog.component';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { EmpHomeComponent } from "./EmpDetails/emp-home/emp-home.component";
+import { ErrorComponent } from "./EmpDetails/error/error.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { EmpMaterialModuleModule } from "./EmpDetails/emp-material-module/emp-material-module.module";
+import { HttpClientModule } from "@angular/common/http";
+import { ViewDialogComponent } from "./EmpDetails/emp-home/view-dialog/view-dialog.component";
+import { EditDialogComponent } from "./EmpDetails/emp-home/edit-dialog/edit-dialog.component";
+import { AddDialogComponent } from "./EmpDetails/emp-home/add-dialog/add-dialog.component";
+import { DeleteDialogComponent } from "./EmpDetails/emp-home/delete-dialog/delete-dialog.component";
+import { ReactiveFormsModule } from "@angular/forms";
+import { EmpCurdServiseService } from "./EmpDetails/emp-curd-servise.service";
+import { PopupMessageComponent } from "./EmpDetails/emp-home/popup-message/popup-message.component";
 
 @NgModule({
   declarations: [
@@ -24,7 +24,7 @@ import { PopupDialogComponent } from './EmpDetails/emp-home/popup-dialog/popup-d
     EditDialogComponent,
     AddDialogComponent,
     DeleteDialogComponent,
-    PopupDialogComponent,
+    PopupMessageComponent,
   ],
   imports: [
     BrowserModule,
